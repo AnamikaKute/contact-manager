05d0fc0cd7d06fbd665ab97e692c034a8b262021
Anamika2424 <kuteanamika7@gmail.com>
2025-10-29 09:50:25 +0530

    i have craeted db.json file and completed the UI and i have made the API call

---
+++ contact-manager/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Contact from './Components/Contact/Contact';
+import ContactList from './Components/ContactList/ContactList';
+function App() {
+  return (
+    <div className="App">
+      <Contact/>
+      {/* <ContactList/> */}
+    </div>
+  );
+}
+
+export default App;
